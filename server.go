6705a48bb86a1ccd4f3451e0595a15526a76c1a4
0125nia <1205367274@qq.com>
2024-04-14 16:52:15 +0800

    server.go Handler_update 超时强踢功能

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 // Server server类型 包含ip与port
@@ -70,6 +71,12 @@ func (s *Server) Handler(conn net.Conn) {
 
 	user.Online(s)
 
+	//在用户的Handler() goroutine中添加用户活跃channel,一旦有消息即向该channel发送消息类似激活操作
+	//长时间不发送消息将被认为超时,将被强制关闭用户连接
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
+
+	//接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
 
@@ -89,11 +96,35 @@ func (s *Server) Handler(conn net.Conn) {
 
 			//将得到的消息进行广播
 			user.DoMsg(s, msg)
+
+			//用户的任意消息,代表当前用户是一个活跃的状态
+			isLive <- true
 		}
 	}()
 
 	//当前handler阻塞
-	select {}
+	for {
+		//超时强制踢出功能
+		select {
+		case <-isLive:
+			//当前用户是活跃的,应重置定时器
+			//不做任何操作,由于case穿透会执行下一行判断操作,变相重置了定时器
+		case <-time.After(time.Minute * 5): //在用户的goroutine中添加定时器功能,判断是否超时
+			//执行此case代表已经超时
+			//将当前的User连接强制关闭
+			user.SendMsg("你因超时被踢了")
+
+			//释放该用户所用的资源
+			close(user.C)
+
+			//关闭连接
+			conn.Close()
+
+			//退出当前Handler
+			return //runtime.Goexit()
+		}
+	}
+
 }
 
 // Start 启动服务器的接口
